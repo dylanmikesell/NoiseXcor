afcb402e9da78cc988479930ad89dcee969519ed
Dylan Mikesell <dylan.mikesell@gmail.com>
2019-06-04 13:58:32 -0600

    Updated miniseed example based on new database creation code.

--- Example_mSEED/A0_makeStationDataBase.m
+++ Example_mSEED/A0_makeStationDataBase.m
@@ -9,17 +9,18 @@ addpath('/hammer/SOFTWARE/NoiseXcor/src');
 
 % folder where all files will be written
 project_directory = '/hammer/SOFTWARE/NoiseXcor/Example_mSEED';
-data_directory = fullfile( project_directory, 'DATA'); % where are the data?
 
-% project_directory = '/Volumes/ESlab/Data_02/';
-% % data_directory = fullfile( project_directory, 'DATA2010/201002'); % where are the data?
-% data_directory = project_directory;
+data_directory = fullfile( project_directory, 'DATA'); % where are the data?
 
 database_name  = 'Polenet_db.mat'; % what do you want call your database
 
 file_type       = 'miniseed'; % 'sac', 'seed', 'miniseed'
-data_structure  = 'DMT'; % 'SDS', 'BUD', 'IDDS', 'PDF', 'DMT' (only 'BUD' and 'DMT' currently implemented) -- 'DMT' is for data downloaded with obspyDMT
+data_structure  = 'DMT'; % 'SDS', 'BUD', 'IDDS', 'PDF', 'DMT' (only 'BUD', 'ANT' and 'DMT' currently implemented) -- 'DMT' is for data downloaded with obspyDMT
 channel         = {'BHZ','HHZ'}; % a cell list of any channels to use
+
+% Station coodinates: formatted text file
+% Columns are:  NAME    NET     LAT     LON     ELE
+coordinate_file = './station_coordinates.csv'; % This is the coordinate file (CSV or TXT)
 
 % enter time of earliest data -- MAT file names are relative to this.
 start_date = '2010-01-01 00:00:00'; % ['YYYY-MM-DD HH:MM:SS.FFF']
@@ -34,5 +35,6 @@ stationData = initializeTable( ...
     data_structure, ...
     start_date, ...
     end_date, ...
-    channel ...
+    channel, ...
+    coordinate_file ...
     );
